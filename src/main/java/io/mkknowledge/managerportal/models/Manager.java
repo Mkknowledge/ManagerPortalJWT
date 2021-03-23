803390aa25c16e248b388fdf89c715cb7eb07f70
Mkknowledge <kmayur2124@gmail.com>
2021-03-23 09:39:10 +0530

    added Manager model in portal

---
+++ src/main/java/io/mkknowledge/managerportal/models/Manager.java
@@ -0,0 +1,175 @@
+package io.mkknowledge.managerportal.models;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table( name="manager",
+		uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+public class Manager {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(max = 50)
+	@Email
+	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
+	private String firstName;
+	
+	@NotBlank
+	@Size(max = 20)
+	private String lastName;
+	
+	@NotBlank
+	@Size(max = 120)
+	private String password;
+	
+	@NotBlank
+	@Size(max = 120)
+	private String address;
+	
+	@JsonFormat(pattern="yyy-mm-dd")
+	private Date dob;
+	
+	@NotBlank
+	@Size(max = 20)
+	private String company;
+	
+	@JsonFormat(pattern="yyy-mm-dd")
+	private Date created_At;
+	@JsonFormat(pattern="yyy-mm-dd")
+	private Date updated_At;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(	name = "user_roles", 
+		joinColumns = @JoinColumn(name = "user_id"), 
+		inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+	
+	@PrePersist
+	protected void onCreate() {
+		this.created_At= new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updated_At = new Date();
+	}
+
+	public Manager() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public Date getCreated_At() {
+		return created_At;
+	}
+
+	public void setCreated_At(Date created_At) {
+		this.created_At = created_At;
+	}
+
+	public Date getUpdated_At() {
+		return updated_At;
+	}
+
+	public void setUpdated_At(Date updated_At) {
+		this.updated_At = updated_At;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	
+}
